ea8b807ba97570ca10b985cc4b3fc974494d2b71
Athanasios Voivodas <athanasios.voivodas@isignthis.com>
2019-10-04 11:25:42 +0300

    Implements a jwks endpoint.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const jose = require('jose');
+const express = require('express')
+const app = express()
+const port = 3000
 
 const {
   JWE,   // JSON Web Encryption (JWE) 
@@ -8,3 +11,18 @@ const {
   JWT,   // JSON Web Token (JWT)
   errors // errors utilized by jose
 } = jose;
+
+const key1 = JWK.generateSync("RSA");
+const key2 = JWK.generateSync("RSA");
+const signingKey = JWK.generateSync('RSA', 2048, { use: 'sig', alg: 'PS256' })
+
+const keystore = new jose.JWKS.KeyStore(key1, key2, signingKey);
+
+// console.log(JSON.stringify(keystore.toJWKS(true), null, 4));
+
+app.get('/jwks', (req, res) => res.json(keystore.toJWKS(true)))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
